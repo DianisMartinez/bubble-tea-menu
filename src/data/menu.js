5edee3ea404363780aad5664da114f37b88ce7b4
Diana Gomez <dianagomez192002@gmail.com>
2026-02-07 13:47:07 -0300

    k

--- src/data/menu.js
+++ src/data/menu.js
@@ -3,12 +3,13 @@ export const MENU = [
     title: "RECOMENDADO DE LA CASA",
     items: [
       {
-        name: "Osorno Special",
-        desc: "Té de Ceylán + Matcha + leche + tapioca + Popping de Mango",
         sizes: [
           { size: "350 ml", price: "4.290" },
           { size: "700 ml", price: "5.690" },
         ],
+        name: "Osorno Special",
+        desc: "Té de Ceylán + Matcha + leche + tapioca + Popping de Mango",
+        
     
       },
     ],
