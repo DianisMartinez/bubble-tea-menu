2093b181039cdd97d8ff701e29d3dd128247291f
Diana Gomez <dianagomez192002@gmail.com>
2026-02-10 17:22:03 -0300

    precio aajo del

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -2,23 +2,30 @@ export default function ItemCard({ item }) {
   return (
     <div className="itemCard">
       <div className="itemTop">
-        {/* Izquierda: nombre + desc */}
-        <div className="itemText">
-          <div className="itemName">{item.name}</div>
-          {item.desc ? <div className="itemDesc">{item.desc}</div> : null}
-        </div>
 
-        {/* Derecha: precios */}
+        {/* Nombre + tamaños + desc */}
+        <div className="itemText">
         {item.sizes?.length ? (
-          <div className="sizes">
-            {item.sizes.map((s) => (
-              <div className="priceCol" key={s.size}>
-                <div className="sizeLabel">{s.size}</div>
-                <div className="priceValue">${s.price}</div>
-              </div>
-            ))}
-          </div>
-        ) : null}
+            <div className="sizes">
+              {item.sizes.map((s) => (
+                <div className="priceCol" key={s.size}>
+                  <div className="sizeLabel">{s.size}</div>
+                  <div className="priceValue">${s.price}</div>
+                </div>
+              ))}
+            </div>
+          ) : null}
+
+          {/* Título */}
+          <div className="itemName">{item.name}</div>
+
+
+          {/* Descripción */}
+          {item.desc ? (
+            <div className="itemDesc">{item.desc}</div>
+          ) : null}
+
+        </div>
       </div>
 
       {/* Tags abajo */}
