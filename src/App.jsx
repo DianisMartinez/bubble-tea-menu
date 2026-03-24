477d55df069b7f7fe9bf1a8b1ea47ac09480231f
Diana Gomez <dianagomez192002@gmail.com>
2026-03-24 13:05:48 -0300

    kawaii

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import Menu from "./pages/Menu";
 import "./style/theme.css";
-import { DrinkAI } from "./components/DrinkAI";
+
 
 export default function App() {
   return (
     <>
       <Menu />
-      <DrinkAI />
     </>
   );
 }
